9ac3417ddc263965259c2af6820d67aa75ec7f27
Aysan Isayo <aisayo@icloud.com>
2021-08-04 09:38:03 -0700

    working on solution

---
+++ index.js
@@ -0,0 +1,32 @@
+// What are functions and why are they important to programming?
+// Reusable code
+// Organized code
+// Building blocks of JS programming
+// allows us to structure larger programs
+
+
+// What are the different ways to define functions?
+// using function keyword
+// arrow functions
+// function expression
+// anonymous functions 
+
+
+// Go over function anatomy
+// parameters
+// body 
+
+
+// What is the difference between defining and invoking a function?
+
+// function parameters vs arguments
+
+// functions return value vs undefined
+
+
+// refresher on what a scope is
+// global scope vs local (function) scope
+// go over when two scopes share the same variable name, will return its own scope variable value
+
+
+
