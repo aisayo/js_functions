184aa6bdc21ee220491fc7b99d8bd8d188010829
Aysan Isayo <aisayo@icloud.com>
2021-08-05 09:24:17 -0700

    closures and callback functions

--- index.js
+++ index.js
@@ -4,29 +4,54 @@
 // Building blocks of JS programming
 // allows us to structure larger programs
 
-
 // What are the different ways to define functions?
-// using function keyword
-// arrow functions
-// function expression
-// anonymous functions 
 
+// using function keyword/function declaration - point out that this creates a global scope
+// console.log(printPokemonName())
+// function printPokemonName(){
+//     console.log('Hi, I\'m Pikachu!')
+//     return 'Hi, I\'m Pikachu!'
+// }
+
+// function expression
+// anonymous functions
+// console.log(printPokemonName())
+// const printPokemonName = function(){
+//     console.log('Hi, I\'m Pikachu!')
+//     return 'Hi, I\'m Pikachu!'
+// }
+
+// arrow functions
+// const printPokemonName = () => {
+//     console.log('Hi, I\'m Pikachu!')
+//     return 'Hi, I\'m Pikachu!'
+// }
 
 // Go over function anatomy
 // parameters
-// body 
+// body
 
+// function printPokemonName(parameter){
+//     'Hi, I\'m Pikachu!'
+// }
 
 // What is the difference between defining and invoking a function?
+// reference vs invokation
+ 
+// printPokemonName
+// printPokemonName()
 
 // function parameters vs arguments
 
 // functions return value vs undefined
 
-
 // refresher on what a scope is
 // global scope vs local (function) scope
 // go over when two scopes share the same variable name, will return its own scope variable value
+// Nested functions scopes/lexical scoping/closures
 
-
-
+// Callback functions
+// What does it mean for a language to have first-class functions?
+// Store function in a variable
+// Invoke function from variable
+// Use function stored in variable to pass as an argument
